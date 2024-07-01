684e6f64a6ca246fc0a9b8220381a782f5263165
Sanieeme <shoneesani@gmail.com>
2024-07-01 17:42:38 +0000

    s

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListudentIds(students) {
-    if (!Array.isArray(students)) {
-       return [];
-    }
-    const ids = students.map((x) => x.id);
-    return ids;
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  const ids = students.map((x) => x.id);
+  return ids;
 }
