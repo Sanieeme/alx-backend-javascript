0546ca4459db608e7f3a121c82b917cf10b5cbf0
sanieeme <shoneesani@gmail.com>
2024-10-04 21:16:36 +0000

    lint on 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,21 @@
 /**
  * Program named 1-stdin.js that will be executed through command line
  */
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
   console.log(`Your name is: ${name}`);
 
   if (!process.stdin.isTTY) {
-    console.log("This important software is now closing");
+    console.log('This important software is now closing');
   }
 
   process.exit();
 });
 
 process.stdin.on('end', () => {
-  console.log("This important software is now closing");
+  console.log('This important software is now closing');
 });
 
 if (process.stdin.isTTY) {
