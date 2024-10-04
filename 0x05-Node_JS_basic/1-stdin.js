c45af3f0444f98f0e9c1c6d7a45578243fd7ada2
sanieeme <shoneesani@gmail.com>
2024-10-04 21:10:13 +0000

    update 1-stin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,16 +6,20 @@ console.log("Welcome to Holberton School, what is your name?");
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.exit();
 
-  // Check if the input is from a pipe (not interactive)
   if (!process.stdin.isTTY) {
     console.log("This important software is now closing");
-    process.exit();
   }
+
+  process.exit();
 });
 
-// If in interactive mode, keep the process open to wait for input
+process.stdin.on('end', () => {
+  console.log("This important software is now closing");
+});
+
 if (process.stdin.isTTY) {
   process.stdin.resume();
 }
+
+module.exports = process.stdin;
